268731888f10f7d1c3688de4b55d31e62c5222ba
Vadim <korobeinikovv@mail.ru>
2018-12-13 01:18:00 +0300

    Moved notification controls to separate component

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
-import { Notifications, NotificationsStore, Button } from "../components";
+import { Notifications, NotificationsStore } from "../components";
+import { NotificationControls } from "./shared";
 
 const styles = StyleSheet.create({
   mainContainer: {
@@ -12,10 +13,7 @@ const styles = StyleSheet.create({
     fontSize: 20,
     color: "#d9d9d9"
   },
-  buttonControl: {
-    marginBottom: 10
-  },
-  buttonControlsContainer: {
+  notificationControlsContainer: {
     position: "absolute",
     marginTop: 30,
     marginRight: 5,
@@ -26,83 +24,12 @@ const styles = StyleSheet.create({
 
 const notificationsStore = new NotificationsStore();
 
-const addSuccessNotification = () => {
-  notificationsStore.add({
-    id: Date.now(),
-    type: "success",
-    title: "Lorem ipsum dolor",
-    message: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-  });
-};
-const addInfoNotification = () => {
-  notificationsStore.add({
-    id: Date.now(),
-    type: "info",
-    title: "Lorem ipsum dolor",
-    message: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-  });
-};
-const addWarningNotification = () => {
-  notificationsStore.add({
-    id: Date.now(),
-    type: "warning",
-    title: "Lorem ipsum dolor",
-    message: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-  });
-};
-const addErrorNotification = () => {
-  notificationsStore.add({
-    id: Date.now(),
-    type: "error",
-    title: "Lorem ipsum dolor",
-    message: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-  });
-};
-
 export const Home = () => {
   return (
     <View style={styles.mainContainer}>
       <Text style={styles.title}>react-native-notification-system</Text>
-      <View style={styles.buttonControlsContainer}>
-        {[
-          {
-            key: "create-success-notification-button",
-            iconName: "check",
-            colorPrimary: "#dff0d8",
-            colorAccent: "#3c763d",
-            onPress: addSuccessNotification
-          },
-          {
-            key: "create-info-notification-button",
-            iconName: "info",
-            colorPrimary: "#d9edf7",
-            colorAccent: "#31708f",
-            onPress: addInfoNotification
-          },
-          {
-            key: "create-warning-notification-button",
-            iconName: "exclamation",
-            colorPrimary: "#fcf8e3",
-            colorAccent: "#8a6d3b",
-            onPress: addWarningNotification
-          },
-          {
-            key: "create-error-notification-button",
-            iconName: "times",
-            colorPrimary: "#f2dede",
-            colorAccent: "#a94442",
-            onPress: addErrorNotification
-          }
-        ].map(b => (
-          <View key={b.key} style={styles.buttonControl}>
-            <Button
-              iconName={b.iconName}
-              colorPrimary={b.colorPrimary}
-              colorAccent={b.colorAccent}
-              onPress={b.onPress}
-            />
-          </View>
-        ))}
+      <View style={styles.notificationControlsContainer}>
+        <NotificationControls store={notificationsStore} />
       </View>
       <Notifications store={notificationsStore} />
     </View>
---
+++ src/screens/shared/NotificationControls.js
@@ -0,0 +1,94 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { View, StyleSheet } from "react-native";
+import { Button, NotificationsStore } from "../../components";
+
+const styles = StyleSheet.create({
+  buttonControl: {
+    marginBottom: 10
+  }
+});
+
+const addSuccessNotification = notificationsStore => {
+  notificationsStore.add({
+    id: Date.now(),
+    type: "success",
+    title: "Lorem ipsum dolor",
+    message: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+  });
+};
+const addInfoNotification = notificationsStore => {
+  notificationsStore.add({
+    id: Date.now(),
+    type: "info",
+    title: "Lorem ipsum dolor",
+    message: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+  });
+};
+const addWarningNotification = notificationsStore => {
+  notificationsStore.add({
+    id: Date.now(),
+    type: "warning",
+    title: "Lorem ipsum dolor",
+    message: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+  });
+};
+const addErrorNotification = notificationsStore => {
+  notificationsStore.add({
+    id: Date.now(),
+    type: "error",
+    title: "Lorem ipsum dolor",
+    message: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+  });
+};
+
+export const NotificationControls = props => {
+  const { store } = props;
+  return (
+    <React.Fragment>
+      {[
+        {
+          key: "create-success-notification-button",
+          iconName: "check",
+          colorPrimary: "#dff0d8",
+          colorAccent: "#3c763d",
+          onPress: addSuccessNotification
+        },
+        {
+          key: "create-info-notification-button",
+          iconName: "info",
+          colorPrimary: "#d9edf7",
+          colorAccent: "#31708f",
+          onPress: addInfoNotification
+        },
+        {
+          key: "create-warning-notification-button",
+          iconName: "exclamation",
+          colorPrimary: "#fcf8e3",
+          colorAccent: "#8a6d3b",
+          onPress: addWarningNotification
+        },
+        {
+          key: "create-error-notification-button",
+          iconName: "times",
+          colorPrimary: "#f2dede",
+          colorAccent: "#a94442",
+          onPress: addErrorNotification
+        }
+      ].map(b => (
+        <View key={b.key} style={styles.buttonControl}>
+          <Button
+            iconName={b.iconName}
+            colorPrimary={b.colorPrimary}
+            colorAccent={b.colorAccent}
+            onPress={() => b.onPress(store)}
+          />
+        </View>
+      ))}
+    </React.Fragment>
+  );
+};
+
+NotificationControls.propType = {
+  store: PropTypes.instanceOf(NotificationsStore).isRequired
+};
